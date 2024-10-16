2d27944e4d800b078556791299a9fe2059772b80
Luke van der Harg <9025126@student.zadkine.nl>
2024-10-16 15:23:29 +0200

    Opdr_3

---
+++ Week2/app.js
@@ -0,0 +1,7 @@
+document.getElementById('Text').style.color = 'blue'
+document.getElementById("Text").innerText = "Nieuwe tekst"
+
+function changeText() {
+  document.getElementById('Text').style.color = 'red'
+  document.getElementById("Text").innerText = "Nieuwe tekst 2"
+}
