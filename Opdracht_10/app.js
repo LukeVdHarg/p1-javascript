00df136997a1a91a30a644c06668a5a7ac7427ff
Luke van der Harg <9025126@student.zadkine.nl>
2024-10-18 11:19:55 +0200

    Opdracht_11

--- Opdracht_10/app.js
+++ Opdracht_10/app.js
@@ -13,25 +13,30 @@ function calculate(calculatorValue) {
   Number1 = Number(document.getElementById("input1").value)
   Number2 = Number(document.getElementById("input2").value)
   console.log(Number1, Number2, calculatorValue)
-  if (calculatorValue == 1) {
-    Antwoord = Number1 + Number2
-    reset()
-    document.getElementById("addButton").style.backgroundColor = "green"
+  if (Number1 > 0 && Number2 > 0) {
+    if (calculatorValue == 1) {
+      Antwoord = Number1 + Number2
+      reset()
+      document.getElementById("addButton").style.backgroundColor = "green"
+    }
+    else if (calculatorValue == 2){
+      Antwoord = Number1 - Number2
+      reset()
+      document.getElementById("subtractButton").style.backgroundColor = "green"
+    }
+    else if (calculatorValue == 3){
+      Antwoord = Number1 * Number2
+      reset()
+      document.getElementById("multiplyButton").style.backgroundColor = "green"
+    }
+    else if (calculatorValue == 4) {
+      Antwoord = Number1 / Number2
+      reset()
+      document.getElementById("divideButton").style.backgroundColor = "green"
+    }
+    document.getElementById("Antwoord").innerText = "Het antwoord is: " + Antwoord
   }
-  else if (calculatorValue == 2){
-    Antwoord = Number1 - Number2
-    reset()
-    document.getElementById("subtractButton").style.backgroundColor = "green"
+  else {
+    document.getElementById("Antwoord").innerText = "Het getal is te laag"
   }
-  else if (calculatorValue == 3){
-    Antwoord = Number1 * Number2
-    reset()
-    document.getElementById("multiplyButton").style.backgroundColor = "green"
-  }
-  else if (calculatorValue == 4) {
-    Antwoord = Number1 / Number2
-    reset()
-    document.getElementById("divideButton").style.backgroundColor = "green"
-  }
-  document.getElementById("Antwoord").innerText = "Het antwoord is: " + Antwoord
 }
