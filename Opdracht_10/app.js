02461c84e795321fbc150d6434fb6f6721f88345
Luke van der Harg <9025126@student.zadkine.nl>
2024-10-18 11:16:00 +0200

    Opdr_10

--- Opdracht_10/app.js
+++ Opdracht_10/app.js
@@ -1,21 +1,37 @@
 Antwoord
 
+function reset(){
+  document.getElementById("addButton").style.backgroundColor = "gray"
+  document.getElementById("subtractButton").style.backgroundColor = "gray"
+  document.getElementById("multiplyButton").style.backgroundColor = "gray"
+  document.getElementById("divideButton").style.backgroundColor = "gray"
+
+  document.getElementById("Antwoord").innerText = "Rekenmachine"
+}
+
 function calculate(calculatorValue) {
   Number1 = Number(document.getElementById("input1").value)
   Number2 = Number(document.getElementById("input2").value)
   console.log(Number1, Number2, calculatorValue)
   if (calculatorValue == 1) {
     Antwoord = Number1 + Number2
+    reset()
+    document.getElementById("addButton").style.backgroundColor = "green"
   }
   else if (calculatorValue == 2){
     Antwoord = Number1 - Number2
+    reset()
+    document.getElementById("subtractButton").style.backgroundColor = "green"
   }
   else if (calculatorValue == 3){
     Antwoord = Number1 * Number2
+    reset()
+    document.getElementById("multiplyButton").style.backgroundColor = "green"
   }
   else if (calculatorValue == 4) {
     Antwoord = Number1 / Number2
+    reset()
+    document.getElementById("divideButton").style.backgroundColor = "green"
   }
-  // Antwoord = Antwoord.toFixed(2) / Antwoord = Math.round(Antwoord)
   document.getElementById("Antwoord").innerText = "Het antwoord is: " + Antwoord
 }
